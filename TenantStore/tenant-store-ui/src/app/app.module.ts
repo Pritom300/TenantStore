f23602b480ea27c3f1820ab2928429fc294eab94
pritom300 <pritomsarkar300@gmail.com>
2025-12-18 13:10:05 +0600

    Created Angular Project Without Standalone, Created models and components

---
+++ TenantStore/tenant-store-ui/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { ProductListComponent } from './features/products/product-list/product-list.component';
+import { ProductFormComponent } from './features/products/product-form/product-form.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProductListComponent,
+    ProductFormComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
